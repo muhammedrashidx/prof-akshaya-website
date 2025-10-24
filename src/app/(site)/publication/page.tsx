1e8abf51e4c04d6159e96b3cba912c6d02a6be98
muhammed rashid <muhammedrashid.edu@gmail.com>
2025-10-24 21:34:16 +0530

    updated publications

--- src/app/(site)/publication/page.tsx
+++ src/app/(site)/publication/page.tsx
@@ -26,10 +26,12 @@ const PublicationPage = () => {
                           DOES INCREASING HORIZONTAL RESOLUTION IMPROVE THE SIMULATION OF INTENSE TROPICAL RAINFALL IN GFDL&apos;S AM4 MODEL?
                         </span>
                         <a 
-                          href="#" 
+                          href="https://essopenarchive.org/users/575191/articles/671703-does-increasing-horizontal-resolution-improve-the-simulation-of-intense-tropical-rainfall" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -37,7 +39,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya C. Nikumbh</span>, Pu Lin, David Paynter, Yi Ming
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          in review, Preprint <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          in review, Preprint <a href="https://essopenarchive.org/users/575191/articles/671703-does-increasing-horizontal-resolution-improve-the-simulation-of-intense-tropical-rainfall" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -56,10 +58,12 @@ const PublicationPage = () => {
                           ATMOSPHERE AND OCEAN ENERGY TRANSPORT IN EXTREME WARMING SCENARIOS
                         </span>
                         <a 
-                          href="#" 
+                          href="https://journals.plos.org/climate/article?id=10.1371/journal.pclm.0000343" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -67,7 +71,7 @@ const PublicationPage = () => {
                               Alyssa Poletti, Dargan M. W. Frierson, Travis Aerenson, <span className="font-bold">Akshaya C. Nikumbh</span>, Rachel Carroll, William Henshaw
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">PLOS Climate</span>, February 2024. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">PLOS Climate</span>, February 2024. <a href="https://journals.plos.org/climate/article?id=10.1371/journal.pclm.0000343" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -86,10 +90,12 @@ const PublicationPage = () => {
                           THE ROLE OF NORTH ATLANTIC BLOCKING HIGH DURING LARGE-SCALE HEAVY RAINFALL EVENTS OVER CENTRAL INDIA
                         </span>
                         <a 
-                          href="#" 
+                          href="https://journals.ametsoc.org/view/journals/atsc/80/7/JAS-D-22-0185.1.xml" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -97,7 +103,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya Nikumbh</span>, A. B. S. Thakur, A. Chakraborty, G. S. Bhat, J. Sukhatme
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">Journal of Atmospheric Sciences</span>, March 2023. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">Journal of Atmospheric Sciences</span>, March 2023. <a href="https://journals.ametsoc.org/view/journals/atsc/80/7/JAS-D-22-0185.1.xml" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -116,10 +122,12 @@ const PublicationPage = () => {
                           MULTISCALE INTERACTIONS BETWEEN MONSOON INTRA-SEASONAL OSCILLATIONS AND LOW-PRESSURE SYSTEMS THAT PRODUCE HEAVY RAINFALL EVENTS OF DIFFERENT SPATIAL EXTENTS
                         </span>
                         <a 
-                          href="#" 
+                          href="https://journals.ametsoc.org/view/journals/clim/aop/JCLI-D-21-0231.1/JCLI-D-21-0231.1.xml" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -127,7 +135,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya Nikumbh</span>, A. Chakraborty, G. S. Bhat, D. M. W. Frierson
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">Journal of Climate</span>, September 2021. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">Journal of Climate</span>, September 2021. <a href="https://journals.ametsoc.org/view/journals/clim/aop/JCLI-D-21-0231.1/JCLI-D-21-0231.1.xml" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -146,10 +154,12 @@ const PublicationPage = () => {
                           LARGE-SCALE EXTREME RAINFALL-PRODUCING SYNOPTIC SYSTEMS OF THE INDIAN SUMMER MONSOON
                         </span>
                         <a 
-                          href="#" 
+                          href="https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2020GL088403" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -157,7 +167,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya Nikumbh</span>, A. Chakraborty, G. S. Bhat, D. M. W. Frierson
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">GRL</span>, May 2020. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">GRL</span>, May 2020. <a href="https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2020GL088403" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -176,10 +186,12 @@ const PublicationPage = () => {
                           RECENT SPATIAL AGGREGATION TENDENCY OF RAINFALL EXTREMES OVER INDIA
                         </span>
                         <a 
-                          href="#" 
+                          href="https://www.nature.com/articles/s41598-019-46719-2" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -187,7 +199,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya Nikumbh</span>, A. Chakraborty, G. S. Bhat
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">Scientific Reports</span>, June 2019. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">Scientific Reports</span>, June 2019. <a href="https://www.nature.com/articles/s41598-019-46719-2" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
@@ -206,10 +218,12 @@ const PublicationPage = () => {
                           CLOUD FRACTION RETRIEVAL AND ITS VARIABILITY DURING DAYTIME FROM GROUND-BASED SKY IMAGERY OVER A TROPICAL STATION IN INDIA
                         </span>
                         <a 
-                          href="#" 
+                          href="https://www.sciencedirect.com/science/article/abs/pii/S1364682618307065" 
+                          target="_blank"
+                          rel="noopener noreferrer"
                           className="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400"
                         >
-                          [PDF]
+                          
                         </a>
                       </div>
                       <div className="ml-4 text-sm text-dark dark:text-white" style={{ fontFamily: 'EB Garamond, serif', fontSize: '1.1rem' }}>
@@ -217,7 +231,7 @@ const PublicationPage = () => {
                               <span className="font-bold">Akshaya Nikumbh</span>, B. PadmaKumari, Sneha Sunil
                             </p>
                         <p className="italic text-gray-600 dark:text-gray-400">
-                          <span className="italic">Journal of Atmospheric and Solar-Terrestrial Physics</span>, May 2019. <a href="#" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
+                          <span className="italic">Journal of Atmospheric and Solar-Terrestrial Physics</span>, May 2019. <a href="https://www.sciencedirect.com/science/article/abs/pii/S1364682618307065" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 dark:text-blue-400">Link</a>
                         </p>
                       </div>
                     </div>
